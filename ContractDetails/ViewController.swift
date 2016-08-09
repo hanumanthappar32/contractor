23fa7cf454834440d29c139840de37f393589c4a
Ramesh <hanumanthappar32@gmail.com>
2016-08-09 13:12:44 +0530

    Getting input from user by using alert UI

--- ContractDetails/ViewController.swift
+++ ContractDetails/ViewController.swift
@@ -7,10 +7,30 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController, UITableViewDataSource {
     
-    var departements = ["PWD","MI","ZP","TP", "Irrigation"]
+    @IBOutlet weak var myTableview: UITableView!
+    var departements:[String] = []
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Departements"
+        
+        myTableview.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
@@ -20,28 +40,33 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
         cell.textLabel?.text = departements[indexPath.row]
         return cell
     }
     
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func addDepartement(sender: AnyObject) {
         
+        let alert = UIAlertController(title: "Add Departement", message: "Add New Departement", preferredStyle: .Alert)
+        let save = UIAlertAction (title: "Save", style: .Default) {
+            (action:UIAlertAction) -> Void in
+            let textField = alert.textFields![0] as UITextField
+            self.departements.append(textField.text!)
+            self.myTableview.reloadData()
+        }
         
+        let cancel = UIAlertAction (title: "Cancel", style: .Default) {
+            (action:UIAlertAction) -> Void in
+        }
         
-        
-        
+        alert.addTextFieldWithConfigurationHandler { (textfield:UITextField) -> Void in
+        }
+        alert.addAction(save)
+        alert.addAction(cancel)
+        presentViewController(alert, animated: true, completion: nil)
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
