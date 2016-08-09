f1eda26b1afc1c59cb70805d20bd82227822b83a
Ramesh <hanumanthappar32@gmail.com>
2016-08-09 11:01:09 +0530

    Initial Commit

---
+++ ContractDetails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContractDetails
+//
+//  Created by Ramesh H on 09/08/16.
+//  Copyright © 2016 kpwd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
