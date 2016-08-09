7fe89b8c376a2426c66d150d1f8d008481f9f0ca
Ramesh <hanumanthappar32@gmail.com>
2016-08-09 12:16:59 +0530

    reusable cell

--- ContractDetails/ViewController.swift
+++ ContractDetails/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var departements = ["PWD","MI","ZP","TP", "Irrigation"]
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return departements.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = departements[indexPath.row]
+        return cell
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
